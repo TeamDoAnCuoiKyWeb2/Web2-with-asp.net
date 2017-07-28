ddb6d99e8e2905c94544eb3356493087f9987726
DELL-PC\DELL <myngoc3456@gmail.com>
2017-07-28 10:54:28 +0700

    hien thi partial view loai san pham

---
+++ Do An Web Cuoi Ky/1460650_1461691/1460650_1461691/1460650_/Controllers/LoaiSanPhamController.cs
@@ -0,0 +1,91 @@
+﻿using _1460650_.Models.Bus;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace _1460650_.Controllers
+{
+    public class LoaiSanPhamController : Controller
+    {
+        // GET: LoaiSanPham
+        public ActionResult Index(int id)
+        {
+            var dsSanPham = LoaiSPBus.ChiTiet(id);
+            return View(dsSanPham);
+        }
+
+        // GET: LoaiSanPham/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: LoaiSanPham/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: LoaiSanPham/Create
+        [HttpPost]
+        public ActionResult Create(FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add insert logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: LoaiSanPham/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: LoaiSanPham/Edit/5
+        [HttpPost]
+        public ActionResult Edit(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add update logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: LoaiSanPham/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: LoaiSanPham/Delete/5
+        [HttpPost]
+        public ActionResult Delete(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add delete logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
---
+++ Do An Web Cuoi Ky/1460650_1461691/1460650_1461691/1460650_/Models/Bus/LoaiSPBus.cs
@@ -0,0 +1,24 @@
+﻿using DienThoaiShopConnection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace _1460650_.Models.Bus
+{
+    public class LoaiSPBus
+    {
+        public static IEnumerable<loaisp> DanhSach()
+        {
+            var db = new DienThoaiShopConnectionDB();
+            return db.Query<loaisp>("select * from loaisp");
+
+        }
+        public static IEnumerable<sanpham> ChiTiet(int id)
+        {
+            var db = new DienThoaiShopConnectionDB();
+            return db.Query<sanpham>("select * from sanpham where MaLoai = '" + id + "'");
+
+        }
+    }
+}
